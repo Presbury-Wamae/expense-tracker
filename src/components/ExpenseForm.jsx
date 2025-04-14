b0c2d3979f6e47e1260ff02e5f96a7cd141222bc
Presbury-Wamae <wamaepresbury034@gmail.com>
2025-04-14 18:44:22 +0300

    Add ExpenseForm and SearchBar components for expense management

---
+++ src/components/ExpenseForm.jsx
@@ -0,0 +1,11 @@
+function ExpenseForm() {
+    return (
+      <form>
+        <h2>Add Expense</h2>
+        {/* Form fields go here */}
+      </form>
+    );
+  }
+  
+  export default ExpenseForm;
+  
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+function SearchBar() {
+    return (
+      <div>
+        <input type="text" placeholder="Search expenses..." />
+      </div>
+    );
+  }
+  
+  export default SearchBar;
+  
