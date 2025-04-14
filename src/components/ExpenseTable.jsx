311a235a4ca5d426471e8d3bfd896a3c2ca14fa7
Presbury-Wamae <wamaepresbury034@gmail.com>
2025-04-14 18:47:43 +0300

    Implement ExpenseTable component to display a list of expenses

---
+++ src/components/ExpenseTable.jsx
@@ -0,0 +1,22 @@
+import ExpenseRow from "./ExpenseRow";
+
+function ExpenseTable({ expenses }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expense, index) => (
+          <ExpenseRow key={index} expense={expense} />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default ExpenseTable;
