fca49bb5c8c8d42233fc9f0596bd027618796c72
Presbury-Wamae <wamaepresbury034@gmail.com>
2025-04-14 19:01:33 +0300

    Refactor ExpenseForm to handle expense submission and reset form fields

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,8 +1,64 @@
-function ExpenseForm() {
+import { useState } from "react";
+function ExpenseForm({ onAddExpense }) {
+        const [description, setDescription] = useState("");
+        const [amount, setAmount] = useState("");
+        const [category, setCategory] = useState("");
+      
+        const handleSubmit = (e) => {
+          e.preventDefault();
+      
+          const newExpense = {
+            description,
+            amount: parseFloat(amount),
+            category
+          };
+      
+          onAddExpense(newExpense); // send new expense to App
+      
+          // Reset form fields
+          setDescription("");
+          setAmount("");
+          setCategory("");
+        };
+    
+    
+    
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Add Expense</h2>
-        {/* Form fields go here */}
+        <div>
+            <label>Description: </label>
+            <input
+            type="text"
+            value={description}
+            onChange={(e)=> setDescription(e.target.value)}
+            required
+            />
+        </div>
+        <div>
+            <label>Amount: </label>
+            <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            required
+            />
+        </div>
+        
+    <div>
+        <label>Category: </label>
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          required>
+          <option value="">-- Select Category --</option>
+          <option value="Food">Food</option>
+          <option value="Transport">Transport</option>
+          <option value="Utilities">Utilities</option>
+          <option value="Entertainment">Entertainment</option>
+        </select>
+    </div>
+    <button type="submit">Add Expense</button>
       </form>
     );
   }
