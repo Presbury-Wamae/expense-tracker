b9b7a847a5f724096b1bc4cfe3b15a58f5a7810a
Presbury-Wamae <wamaepresbury034@gmail.com>
2025-04-14 19:20:08 +0300

    Add delete button to ExpenseRow and format amount display

--- src/components/ExpenseRow.jsx
+++ src/components/ExpenseRow.jsx
@@ -1,9 +1,12 @@
-function ExpenseRow({ expense }) {
+function ExpenseRow({ expense, onDelete }) {
     return (
       <tr>
         <td>{expense.description}</td>
-        <td>{expense.amount}</td>
+        <td>{expense.amount.toFixed(2)}</td>
         <td>{expense.category}</td>
+        <td>
+          <button onClick={onDelete}>Delete</button>
+        </td>
       </tr>
     );
   }
